5ff448d5c265d4b9acfd88fbf289ee5b45d6ab8b
aiaiaiai1 <cvc100326@naver.com>
2025-10-29 21:31:11 +0900

    feat: 카카오 소셜 로그인을 통한 엑세스 토큰 발급 api 기능 구현

--- src/main/java/guesthouse/oauth2/controller/Oauth2Controller.java
+++ src/main/java/guesthouse/oauth2/controller/Oauth2Controller.java
@@ -1,21 +1,44 @@
 package guesthouse.oauth2.controller;
 
+import guesthouse.oauth2.domain.vo.Provider;
 import guesthouse.oauth2.dto.response.KaKaoLoginUriResponse;
+import guesthouse.oauth2.service.AuthService;
 import guesthouse.oauth2.service.KaKaoOAuth2Service;
+import guesthouse.oauth2.service.KaKaoUserInfoResponse;
+import guesthouse.oauth2.service.KakaoTokenResponse;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
 public class Oauth2Controller {
 
+    private final AuthService authService;
     private final KaKaoOAuth2Service kaKaoOAuth2Service;
 
+    @Value("${oauth2.base-redirect-uri}")
+    private String redirectBaseUri;
+
     @GetMapping("/api/v1/oauth/kakao/login")
-    ResponseEntity<KaKaoLoginUriResponse> getKakaoLoginUri() {
+    public ResponseEntity<KaKaoLoginUriResponse> getKakaoLoginUri() {
         String loginUri = kaKaoOAuth2Service.getLoginUri();
         return ResponseEntity.ok(new KaKaoLoginUriResponse(loginUri));
     }
+
+    @GetMapping("/api/v1/oauth/kakao/callback")
+    public void login(@RequestParam("code") String code, HttpServletResponse response) throws IOException {
+        KakaoTokenResponse kakaoResponse = kaKaoOAuth2Service.getToken(code);
+        KaKaoUserInfoResponse userInfo = kaKaoOAuth2Service.getUserInfo(kakaoResponse.accessToken());
+        String accessToken = authService.loginWithProvider(userInfo.id(), Provider.KAKAO);
+        String redirectUri = redirectBaseUri + accessToken;
+        response.sendRedirect(redirectUri);
+    }
+
 }
