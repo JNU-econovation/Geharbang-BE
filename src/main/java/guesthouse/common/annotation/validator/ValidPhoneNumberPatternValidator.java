950c5adbd7224e15970dbb25d0a4ec505cf3f93b
Woojae-Jeong <woo6388@naver.com>
2025-11-02 01:03:19 +0900

    feat: @ValidPhoneNumberPattern 커스텀 어노테이션 구현

---
+++ src/main/java/guesthouse/common/annotation/validator/ValidPhoneNumberPatternValidator.java
@@ -0,0 +1,18 @@
+package guesthouse.common.annotation.validator;
+
+
+import guesthouse.common.annotation.ValidPhoneNumberPattern;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.util.regex.Pattern;
+
+
+public class ValidPhoneNumberPatternValidator implements ConstraintValidator<ValidPhoneNumberPattern,String> {
+    private static final Pattern PHONE_NUMBER_PATTERN = Pattern.compile("^010-?\\d{4}-?\\d{4}$");
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        return PHONE_NUMBER_PATTERN.matcher(value).matches();
+    }
+}
---
+++ src/main/java/guesthouse/common/exception/message/ValidationMessage.java
@@ -0,0 +1,6 @@
+package guesthouse.common.exception.message;
+
+public class ValidationMessage {
+    public static final String NOT_VALID_PHONE_NUMBER_PATTERN = "핸드폰 번호 양식에 맞지 않습니다.";
+    public static final String SELF_INTRODUCTION_LENGTH_EXCEEDED = "자기소개 내용은 최대 300글자까지만 가능합니다";
+}
