c64f6594607c086b6203fabc274955f429e60aa3
Woojae-Jeong <woo6388@naver.com>
2025-11-02 00:57:58 +0900

    feat: UserService 구현
    유저의 개인정보와 이미지url 업데이트 메소드도 구현

---
+++ src/main/java/guesthouse/user/service/UserService.java
@@ -0,0 +1,38 @@
+package guesthouse.user.service;
+
+import guesthouse.user.domain.vo.Gender;
+import guesthouse.user.exception.UserException;
+import guesthouse.user.domain.model.User;
+import guesthouse.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Transactional(readOnly = true)
+    public User findById(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(UserException::new);
+    }
+
+    public void updatePersonalInfo(String name,
+                                   String phoneNumber,
+                                   LocalDate birthDate,
+                                   Gender gender,
+                                   Long userId) {
+        User user = findById(userId);
+        user.updatePersonalInfo(name, phoneNumber,birthDate, gender);
+    }
+
+    public void updateImageUrl(String imageUrl, Long userId) {
+        User user = findById(userId);
+        user.updateImageUrl(imageUrl);
+    }
+}
