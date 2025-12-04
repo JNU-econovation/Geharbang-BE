25f9c5bd50535d0d782b748b798736701d9d5284
aiaiaiai1 <cvc100326@naver.com>
2025-12-04 23:15:12 +0900

    feat: 여러 이미지 저장 api 구현

--- src/main/java/guesthouse/application/controller/ImageController.java
+++ src/main/java/guesthouse/application/controller/ImageController.java
@@ -1,29 +1,39 @@
 package guesthouse.application.controller;
 
 import guesthouse.application.dto.response.ApplicationImageSaveResponse;
+import guesthouse.application.dto.response.ImagesSaveResponse;
 import guesthouse.application.service.ImageService;
 import guesthouse.common.annotation.UserId;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-@RequestMapping("/api/v1/application/images")
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 public class ImageController {
 
     private final ImageService imageService;
 
-    @PostMapping
+    @PostMapping("/api/v1/application/images")
     public ResponseEntity<ApplicationImageSaveResponse> saveApplicationImage(
             @RequestParam MultipartFile image,
             @UserId Long userId
     ) {
         String imageUrl = imageService.saveImage(image, userId);
         return ResponseEntity.ok(new ApplicationImageSaveResponse(imageUrl));
+    }
+
+    @PostMapping("/api/v1/images")
+    public ResponseEntity<ImagesSaveResponse> saveApplicationImages(
+            @RequestParam List<MultipartFile> images,
+            @UserId Long userId
+    ) {
+        List<String> imageUrls = imageService.saveImages(images, userId);
+        return ResponseEntity.ok(new ImagesSaveResponse(imageUrls));
     }
 }
