d8c9e081427d8e0c0444cebfc8fa5a1240fbb6b7
aiaiaiai1 <cvc100326@naver.com>
2025-12-04 22:43:54 +0900

    refactor: 메서드명 수정

--- src/main/java/guesthouse/staffrecruitment/service/StaffRecruitmentImageMapper.java
+++ src/main/java/guesthouse/staffrecruitment/service/StaffRecruitmentImageMapper.java
@@ -17,15 +17,15 @@ public final class StaffRecruitmentImageMapper {
         List<StaffRecruitmentImage> images = new ArrayList<>();
 
         List<String> representativeImageUrls = request.representativeImageUrls();
-        images.addAll(createFrom(staffRecruitmentId, representativeImageUrls, StaffRecruitmentImageType.대표이미지));
+        images.addAll(createImages(staffRecruitmentId, representativeImageUrls, StaffRecruitmentImageType.대표이미지));
 
         List<String> contentImageUrls = request.introduction().imageUrls();
-        images.addAll(createFrom(staffRecruitmentId, contentImageUrls, StaffRecruitmentImageType.내용이미지));
+        images.addAll(createImages(staffRecruitmentId, contentImageUrls, StaffRecruitmentImageType.내용이미지));
 
         return images;
     }
 
-    private static List<StaffRecruitmentImage> createFrom(Long staffRecruitmentId, List<String> imageUrls, StaffRecruitmentImageType type) {
+    private static List<StaffRecruitmentImage> createImages(Long staffRecruitmentId, List<String> imageUrls, StaffRecruitmentImageType type) {
         List<StaffRecruitmentImage> images = new ArrayList<>();
         for (int index = 0; index < imageUrls.size(); index++) {
             String representativeImageUrl = imageUrls.get(index);
