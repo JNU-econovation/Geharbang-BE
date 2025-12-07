9fddb9602e4575a3fa6bf615642da034f1e58b2e
aiaiaiai1 <cvc100326@naver.com>
2025-12-07 14:04:48 +0900

    refactor: 생성자 작성 대신 롬복 어노테이션 이용

--- src/main/java/guesthouse/staffrecruitment/domain/model/StaffRecruitmentQuestion.java
+++ src/main/java/guesthouse/staffrecruitment/domain/model/StaffRecruitmentQuestion.java
@@ -4,14 +4,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Getter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 @Builder
 public class StaffRecruitmentQuestion {
     @Id
@@ -19,10 +17,4 @@ public class StaffRecruitmentQuestion {
     private Long id;
     private Long staffRecruitmentId;
     private String content;
-
-    public StaffRecruitmentQuestion(Long id, Long staffRecruitmentId, String content) {
-        this.id = id;
-        this.staffRecruitmentId = staffRecruitmentId;
-        this.content = content;
-    }
 }
