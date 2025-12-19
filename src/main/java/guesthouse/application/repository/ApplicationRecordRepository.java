3cc11d1286f760720d822bc14cf7eb8c83fd7a62
Woojae-Jeong <woo6388@naver.com>
2025-12-19 22:06:46 +0900

    refactor: 스탭 지원 시 한 번만 지원할 수 있도록 수정

--- src/main/java/guesthouse/application/repository/ApplicationRecordRepository.java
+++ src/main/java/guesthouse/application/repository/ApplicationRecordRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ApplicationRecordRepository extends JpaRepository<ApplicationRecord,Long> {
+    Boolean existsByStaffRecruitmentIdAndUserId(Long recruitmentId, Long userId);
 }
