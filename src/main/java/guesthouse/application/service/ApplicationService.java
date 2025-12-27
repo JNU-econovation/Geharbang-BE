7bcb466ad553bbd81caa366c00afe0b8fa52d420
Woojae-Jeong <woo6388@naver.com>
2025-12-28 03:58:12 +0900

    refactor: 내 공통 지원서 존재 확인 JPA 메소드 수정
    기존에 userId가 아닌 id를 통해 조회를 하고 있는 오류를 발견해 이를 userId로 조회하게끔 수정

--- src/main/java/guesthouse/application/service/ApplicationService.java
+++ src/main/java/guesthouse/application/service/ApplicationService.java
@@ -40,6 +40,6 @@ public class ApplicationService {
   
     @Transactional(readOnly = true)
     public Boolean hasApplication(Long userId) {
-        return applicationRepository.existsById(userId);
+        return applicationRepository.existsByUserId(userId);
     }
 }
