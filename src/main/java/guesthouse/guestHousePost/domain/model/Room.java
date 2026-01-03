8182ad1e54424724cc07231c8256fd63008e4639
Woojae-Jeong <woo6388@naver.com>
2026-01-03 21:37:38 +0900

    style: 대문자 오타 수정

--- src/main/java/guesthouse/guestHousePost/domain/model/Room.java
+++ src/main/java/guesthouse/guestHousePost/domain/model/Room.java
@@ -19,7 +19,7 @@ public class Room {
     private Long id;
 
     @Column(nullable = false)
-    private Long GuestHousePostId;
+    private Long guestHousePostId;
 
     @Column(nullable = false)
     private String name;
