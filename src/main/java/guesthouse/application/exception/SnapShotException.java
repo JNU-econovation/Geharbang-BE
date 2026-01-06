c40810015cc6140dc1ef1ac1de24dd6e84fdc932
aiaiaiai1 <cvc100326@naver.com>
2026-01-07 00:50:26 +0900

    refactor: 기존 에러의 호환성을 위한 작업

--- src/main/java/guesthouse/application/exception/SnapShotException.java
+++ src/main/java/guesthouse/application/exception/SnapShotException.java
@@ -1,6 +1,11 @@
 package guesthouse.application.exception;
 
+import guesthouse.common.exception.ErrorCode;
 import guesthouse.common.exception.GuestHouseException;
 
 public class SnapShotException extends GuestHouseException {
+
+    public SnapShotException() {
+        super(null);
+    }
 }
--- src/main/java/guesthouse/user/exception/UserException.java
+++ src/main/java/guesthouse/user/exception/UserException.java
@@ -1,6 +1,11 @@
 package guesthouse.user.exception;
 
+import guesthouse.common.exception.ErrorCode;
 import guesthouse.common.exception.GuestHouseException;
 
 public class UserException extends GuestHouseException {
+    public UserException() {
+        super(null);
+    }
 }
+
