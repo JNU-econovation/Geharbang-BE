b061e9f9fc793fe739d72491740a0d70e1ea7689
aiaiaiai1 <cvc100326@naver.com>
2026-01-06 14:51:32 +0900

    feat: 게스트하우스 랜덤 조회 기능 추가

--- src/main/java/guesthouse/guestHousePost/domain/repository/GuestHousePostCustomRepository.java
+++ src/main/java/guesthouse/guestHousePost/domain/repository/GuestHousePostCustomRepository.java
@@ -2,6 +2,7 @@ package guesthouse.guestHousePost.domain.repository;
 
 import guesthouse.guestHousePost.domain.model.GuestHousePost;
 import guesthouse.guestHousePost.domain.vo.GuestHouseFilter;
+import guesthouse.staffrecruitment.domain.vo.Region;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
@@ -9,6 +10,8 @@ import java.util.List;
 public interface GuestHousePostCustomRepository {
 
     List<GuestHousePost> searchByFilter(Pageable pageable, GuestHouseFilter filter);
+
+    List<GuestHousePost> findRandom(int count, Region region);
 
 }
 
