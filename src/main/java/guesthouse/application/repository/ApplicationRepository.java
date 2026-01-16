ccc04d5aff1383984848f0f416c50c4791d4e03d
Woojae-Jeong <woo6388@naver.com>
2026-01-17 00:07:33 +0900

    chore: 패키지 import

--- src/main/java/guesthouse/application/repository/ApplicationRepository.java
+++ src/main/java/guesthouse/application/repository/ApplicationRepository.java
@@ -1,6 +1,7 @@
 package guesthouse.application.repository;
 
 import guesthouse.application.domain.model.Application;
+import guesthouse.user.domain.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +11,5 @@ import java.util.Optional;
 public interface ApplicationRepository extends JpaRepository<Application, Long> {
     Boolean existsByUserId(Long userId);
 
-    Optional<Application> findByUser(guesthouse.user.domain.model.User user);
+    Optional<Application> findByUser(User user);
 }
