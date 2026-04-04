b1e0ea65703825aa107aec280e2608e417d494ed
Superkid0714 <kid1412wer@gmail.com>
2026-04-05 05:31:43 +0900

    feat: OpenApiConfig 클래스 추가 및 JWT 인증 설정 구현

---
+++ src/main/java/guesthouse/common/config/OpenApiConfig.java
@@ -0,0 +1,35 @@
+package guesthouse.common.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        String jwtSchemeName = "jwtAuth";
+
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                        .name(jwtSchemeName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT"));
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Geharbang API 명세서")
+                        .version("v1.0.0")
+                        .description("제주 게스트하우스 스텝 매칭 플랫폼 API"))
+                .addSecurityRequirements(securityRequirement)
+                .setComponents(components);
+    }
+}
