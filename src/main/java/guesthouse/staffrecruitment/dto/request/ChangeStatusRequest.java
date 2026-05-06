98d2d23d6c64212195e1c4fe6628c773d0b2f364
Superkid0714 <kid1412wer@gmail.com>
2026-05-07 03:10:58 +0900

    feat: ChangeStatusRequest에 Swagger 주석 및 유효성 검사 추가

--- src/main/java/guesthouse/staffrecruitment/dto/request/ChangeStatusRequest.java
+++ src/main/java/guesthouse/staffrecruitment/dto/request/ChangeStatusRequest.java
@@ -1,8 +1,12 @@
 package guesthouse.staffrecruitment.dto.request;
 
 import guesthouse.staffrecruitment.domain.vo.Status;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 
 public record ChangeStatusRequest(
+        @NotNull
+        @Schema(description = "변경할 공고 상태", example = "ACTIVE")
         Status status
 ) {
 }
