de3c0721d4fea7ef52c064f7b0ca871f4f83310e
Superkid0714 <kid1412wer@gmail.com>
2026-05-10 17:12:31 +0900

    feat: WishService에 guestHousePost 관련 메서드 추가 및 staffRecruitmentId 삭제 메서드 수정

--- src/main/java/guesthouse/wish/service/WishService.java
+++ src/main/java/guesthouse/wish/service/WishService.java
@@ -26,8 +26,30 @@ public class WishService {
     }
 
     @Transactional
+    public void deleteWishByStaffRecruitmentId(Long userId, Long staffRecruitmentId) {
+        wishRepository.deleteByUserIdAndStaffRecruitmentId(userId, staffRecruitmentId);
+    }
+
+    @Transactional(readOnly = true)
+    public Boolean isWishedGuestHousePost(Long userId, Long guestHousePostId) {
+        return wishRepository.existsByUserIdAndGuestHousePostId(userId, guestHousePostId);
+    }
+
+    @Transactional
+    public Long addWishGuestHousePost(Long userId, Long guestHousePostId) {
+        return wishRepository.findByUserIdAndGuestHousePostId(userId, guestHousePostId)
+                .map(Wish::getId)
+                .orElseGet(() -> wishRepository.save(new Wish(userId, guestHousePostId, true)).getId());
+    }
+
+    @Transactional
+    public void deleteWishByGuestHousePostId(Long userId, Long guestHousePostId) {
+        wishRepository.deleteByUserIdAndGuestHousePostId(userId, guestHousePostId);
+    }
+
+    @Transactional
     public void deleteWish(Long userId, Long wishId) {
-        wishRepository.findByIdAndUserId(userId, wishId)
+        wishRepository.findByIdAndUserId(wishId, userId)
                 .ifPresent(wishRepository::delete);
     }
 
